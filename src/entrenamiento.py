373b3872d8b7baaf433a2f9902cce110c48b7bcf
luiss-23 <lblancog23@gmail.com>
2023-12-07 19:37:16 -0400

    OS differentiation

--- src/entrenamiento.py
+++ src/entrenamiento.py
@@ -134,45 +134,44 @@ class Encoder(tf.keras.Model):
         return tf.zeros((self.batch_sz, self.enc_units))
 
 class Decoder(tf.keras.Model):
-  def __init__(self, vocab_size, emb_dim, dec_units, batch_sz):
-    super(Decoder, self).__init__()
-    self.batch_sz = batch_sz 
-    self.dec_units = dec_units 
-    self.attention = BahdanauAttention(self.dec_units)
-    
-    self.embedding = tf.keras.layers.Embedding(vocab_size, emb_dim) 
-    
-    self.gru = tf.keras.layers.GRU(self.dec_units,
-                                   return_sequences=True,
-                                   return_state=True,
-                                   recurrent_initializer='glorot_uniform') 
-    self.fc = tf.keras.layers.Dense(vocab_size)
+    def __init__(self, vocab_size, emb_dim, dec_units, batch_sz):
+        super(Decoder, self).__init__()
+        self.batch_sz = batch_sz 
+        self.dec_units = dec_units 
+        self.attention = BahdanauAttention(self.dec_units)
+        
+        self.embedding = tf.keras.layers.Embedding(vocab_size, emb_dim) 
+        
+        self.gru = tf.keras.layers.GRU(self.dec_units,
+                                    return_sequences=True,
+                                    return_state=True,
+                                    recurrent_initializer='glorot_uniform') 
+        self.fc = tf.keras.layers.Dense(vocab_size)
 
-  def call(self, x, hidden, enc_output):
-    context_vector, attention_weights = self.attention(hidden, enc_output)
-    x = self.embedding(x)
-    x = tf.concat([tf.expand_dims(context_vector, 1), x], axis=-1)
-    output, state = self.gru(x)
-    output = tf.reshape(output, (-1, output.shape[2]))
-    x = self.fc(output)
-    return x, state , attention_weights
+    def call(self, x, hidden, enc_output):
+        context_vector, attention_weights = self.attention(hidden, enc_output)
+        x = self.embedding(x)
+        x = tf.concat([tf.expand_dims(context_vector, 1), x], axis=-1)
+        output, state = self.gru(x)
+        output = tf.reshape(output, (-1, output.shape[2]))
+        x = self.fc(output)
+        return x, state , attention_weights
 
 class BahdanauAttention(tf.keras.layers.Layer):
-  def __init__(self, units):
-    super(BahdanauAttention, self).__init__()
-    self.W1 = tf.keras.layers.Dense(units) # fully-connected dense layer-1
-    self.W2 = tf.keras.layers.Dense(units) # fully-connected dense layer-2
-    self.V = tf.keras.layers.Dense(1) # fully-connected dense layer-3
+    def __init__(self, units):
+        super(BahdanauAttention, self).__init__()
+        self.W1 = tf.keras.layers.Dense(units) # fully-connected dense layer-1
+        self.W2 = tf.keras.layers.Dense(units) # fully-connected dense layer-2
+        self.V = tf.keras.layers.Dense(1) # fully-connected dense layer-3
 
-  def call(self, query, values):
-   
-    query_with_time_axis = tf.expand_dims(query, 1)
-    score = self.V(tf.nn.tanh(self.W1(query_with_time_axis) + self.W2(values)))   
-    attention_weights = tf.nn.softmax(score, axis=1)
-    context_vector = attention_weights * values
-    context_vector = tf.reduce_sum(context_vector, axis=1)
+    def call(self, query, values):
+        query_with_time_axis = tf.expand_dims(query, 1)
+        score = self.V(tf.nn.tanh(self.W1(query_with_time_axis) + self.W2(values)))   
+        attention_weights = tf.nn.softmax(score, axis=1)
+        context_vector = attention_weights * values
+        context_vector = tf.reduce_sum(context_vector, axis=1)
 
-    return context_vector, attention_weights
+        return context_vector, attention_weights
 
 def main():
 
@@ -218,7 +217,10 @@ def main():
     loss_object = tf.keras.losses.SparseCategoricalCrossentropy(
         from_logits=True, reduction='none')
     
-    checkpoint_dir = './training_checkpoints'  
+    if os.name == 'nt':
+        checkpoint_dir = '../training_checkpoints'
+    elif os.name == 'posix':
+        checkpoint_dir = './training_checkpoints'
     checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")  
     checkpoint = CheckpointManager(checkpoint_dir, 
                                    checkpoint_prefix,
